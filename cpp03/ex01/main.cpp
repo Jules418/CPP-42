940b6dd33cabee73ebe5a8c6b7fd2c1978409624
Jules418 <julio34310@gmail.com>
2024-05-30 10:17:40 +0200

    pull

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -3,28 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jbanacze <jbanacze@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jules <jules@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 15:57:20 by jbanacze          #+#    #+#             */
-/*   Updated: 2024/04/30 17:24:02 by jbanacze         ###   ########.fr       */
+/*   Updated: 2024/05/03 20:11:07 by jules            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 #include <iostream>
 
 int main(){
-	ClapTrap c1("CL4P-TP Steward Bot");
+	ClapTrap ct1("CL4P-TP Steward Bot");
 	std::string VH = "Vault Hunters' ego";
+	ScavTrap st1("SC4V-TP");
 
-	c1.attack(VH);
-	c1.takeDamage(9);
-	c1.beRepaired(1);
-	c1.beRepaired(1);
-	c1.beRepaired(1);
-	c1.beRepaired(1);
-	c1.beRepaired(1);
-	c1.takeDamage(10);
-	c1.beRepaired(100);
+	ct1.attack(VH);
+	st1.attack(VH);
+	st1.guardGate();
 	return 0;
 }
